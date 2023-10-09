0f60c227afe23c1712eb4f136932d77ab22076cb
Gustavo Priebe <gu181288@gmail.com>
2023-10-09 01:25:28 -0300

    multi agent

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ openai.api_key = config('OPENAI_API_KEY')
 
 config_list = config_list_from_json(
     env_or_file="OAI_CONFIG_LIST", filter_dict={"model": ["gpt-3.5-turbo"]})
+
 assistant = AssistantAgent("assistant", llm_config={
-                          "config_list": config_list})
+    "config_list": config_list})
+
 user_proxy = UserProxyAgent(
     "user_proxy", code_execution_config={"work_dir": "coding"})
 
